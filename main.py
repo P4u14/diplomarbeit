62b3742d52644bd661ba6e297c826fdc5e8180e8
Paula <paula.schreiber@altavo.eu>
2025-08-20 15:46:05 +0200

    add new data to dvc

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from atlas.refiner.color_patch_refiner import ColorPatchRefiner
+from atlas.selector.bmi_atlas_selector import BmiAtlasSelector
 from atlas.selector.similarity_atlas_selector import SimilarityAtlasSelector
 from atlas.voter.majority_voter import MajorityVoter
+from atlas.voter.weighted_majority_voter import WeightedMajorityVoter
+from preprocessing.blue_color_preprocessor import BlueColorPreprocessor
+from preprocessing.color_preprocessor import ColorPreprocessor
+from preprocessing.dimples_roi_preprocessor import DimplesRoiPreprocessor
 from segmenter.atlas_segmenter import AtlasSegmenter
 from segmenter.segmentation_runner import SegmentationRunner
 
 ATLAS_DIR = "data/Atlas_Data"
 ATLAS_DIR_BMI_PERCENTILE = "data/Atlas_Data_BMI_Percentile"
 TARGET_IMAGES_DIR = "data/Validation_Data_Small"
+IMAGE_INFO_PATH = "data/Info_Sheets/All_Data_Renamed_overview.csv"
+BMI_TABLE_PATH = "data/Info_Sheets/bmi_table_who.csv"
 
 image_segmenter = [
     AtlasSegmenter(
@@ -25,6 +33,15 @@ image_segmenter = [
         segmentation_voter=MajorityVoter(),
         segmentation_refiner=None,
         output_dir="data/Atlas_Experiment02"
+    ),
+    AtlasSegmenter(
+        num_atlases_to_select=5,
+        atlas_dir=ATLAS_DIR,
+        preprocessing_steps=[DimplesRoiPreprocessor(target_ratio=10/7), BlueColorPreprocessor()],
+        atlas_selector=BmiAtlasSelector(image_info_path=IMAGE_INFO_PATH, bmi_table_path=BMI_TABLE_PATH),
+        segmentation_voter=WeightedMajorityVoter(scheme="softmax", temperature=0.02, threshold=0.5),
+        segmentation_refiner=ColorPatchRefiner(color_preprocessor=BlueColorPreprocessor()),
+        output_dir="data/Atlas_Experiment23"
     ),
 ]
 
--- segmenter/segmentation_runner.py
+++ segmenter/segmentation_runner.py
@@ -11,26 +11,29 @@ class SegmentationRunner:
         start_time = time.time()
 
         target_images = self.segmenter.load_target_images(self.target_images_dir)
-        segmented_images = self.segmenter.segment_images(target_images)
-        self.segmenter.save_segmentation(segmented_images)
+        self.segmenter.segment_images(target_images)
 
         end_time = time.time()
         self.save_duration(start_time, end_time, len(target_images))
 
     def save_duration(self, start_time, end_time, num_images):
         total_seconds = end_time - start_time
-        # format total duration h:m:s
+        # format total duration h:m:s.ms
         hrs = int(total_seconds // 3600)
         mins = int((total_seconds % 3600) // 60)
-        secs = int(total_seconds % 60)
-        duration_str = f"{hrs:02d}:{mins:02d}:{secs:02d}"
+        secs_float = total_seconds % 60
+        secs = int(secs_float)
+        millis = int((secs_float - secs) * 1000)
+        duration_str = f"{hrs:02d}:{mins:02d}:{secs:02d}.{millis:03d}"
         # average per image
         if num_images > 0:
             avg_seconds = total_seconds / num_images
             avg_hrs = int(avg_seconds // 3600)
             avg_mins = int((avg_seconds % 3600) // 60)
-            avg_secs = int(avg_seconds % 60)
-            avg_str = f"{avg_hrs:02d}:{avg_mins:02d}:{avg_secs:02d}"
+            avg_secs_float = avg_seconds % 60
+            avg_secs = int(avg_secs_float)
+            avg_millis = int((avg_secs_float - avg_secs) * 1000)
+            avg_str = f"{avg_hrs:02d}:{avg_mins:02d}:{avg_secs:02d}.{avg_millis:03d}"
         else:
             avg_str = "00:00:00"
         # write durations to file in output_dir
